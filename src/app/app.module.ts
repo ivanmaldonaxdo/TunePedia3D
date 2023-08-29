dcceb00a20036a9d71f0c3f476b579b1d7964692
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2023-08-29 18:37:05 -0400

    refactor(app.component): estructura de nav incrustada a page home y fondo global en app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderBaseComponent } from './components/header/header-base/header-base.component';
 import { NavBaseComponent } from './components/header/nav-base/nav-base.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from './pages/home/home.component';
     HeaderBaseComponent,
     NavBaseComponent,
     HomeComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
