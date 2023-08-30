abe9d6a795c506afd9018fb9baa17b0a9cf27da1
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2023-08-30 01:39:17 -0400

    feat(nav-base): added path de instruments al nav, y routerLinkAtive en nav para acual page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { InstrumentosComponent } from './pages/instrumentos/instrumentos.component';
 
-const routes: Routes = [{
-  path:'',component:HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'instruments',
+    component: InstrumentosComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
