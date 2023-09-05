6a91e401f6199be25de4bd3bbd742e86108e4979
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2023-09-05 17:51:05 -0300

    refactor(instrument.component,index,header.html): cambios de apariencia en esos componentes

--- src/app/modules/angular-material/angular-material.module.ts
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MatCardModule
-  ],
-  exports:[
-    CommonModule,
-    MatCardModule
-  ]
-
+  imports: [CommonModule, MatCardModule, MatButtonModule],
+  exports: [CommonModule, MatCardModule, MatButtonModule],
 })
-export class AngularMaterialModule { }
+export class AngularMaterialModule {}
