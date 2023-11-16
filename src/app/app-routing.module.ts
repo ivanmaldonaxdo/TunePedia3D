584185e2bde58dda472d4aedf379eb032d1bf6bd
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2023-11-16 20:32:52 -0300

    refactor(app-routing...ts):cambio de ruta de detalle instrumento, style(instrumentos.component): cambio de margen en items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     component: InstrumentDetailComponent,
   },
   {
-    path:"instrumentmv/:id",
+    path:"instruments/:id",
     component:InstrumentMvComponent
   }
 ];
