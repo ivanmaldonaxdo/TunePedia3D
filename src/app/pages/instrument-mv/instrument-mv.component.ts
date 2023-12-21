c96502395a5ec793d4074b180740835aa6fdb1c9
Ivan Maldonado <ivanmaldonaxdo98@gmail.com>
2023-12-21 15:55:00 -0300

    style(instrument-mv.component):Se mejora responsividad para el cambio de margen en detail list para tamaños desktop y mobile

--- src/app/pages/instrument-mv/instrument-mv.component.ts
+++ src/app/pages/instrument-mv/instrument-mv.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, NgZone, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MusicalInstResp } from 'src/app/models/musicalInstrument';
 import { InstrumentsService } from 'src/app/services/instruments.service';
@@ -8,7 +8,7 @@ import { InstrumentsService } from 'src/app/services/instruments.service';
   templateUrl: './instrument-mv.component.html',
   styleUrls: ['./instrument-mv.component.css'],
 })
-export class InstrumentMvComponent implements OnInit, AfterViewInit{
+export class InstrumentMvComponent implements OnInit, AfterViewInit, OnDestroy{
   slug!: string;
   instrument!: MusicalInstResp;
   urlFile: string = '';
@@ -23,23 +23,54 @@ export class InstrumentMvComponent implements OnInit, AfterViewInit{
   @ViewChild('audioConfig')
   audioConfig!: ElementRef;
 
+  isPhoneScreen: boolean = false;
+  
+  private mediaQueryList!: MediaQueryList;
+  private mediaQueryListener!: () => void;
+
   constructor(
     private route: ActivatedRoute,
     private instService: InstrumentsService,
-    private renderer: Renderer2
-  ) {}
+    private renderer: Renderer2,
+    private cdr: ChangeDetectorRef,  // Importa ChangeDetectorRef
+    private zone: NgZone
+  ) {
+    // Configurar un observador de medios para detectar cambios en la pantalla
+    this.mediaQueryList = window.matchMedia('(max-width: 500px)');
+    this.isPhoneScreen = this.mediaQueryList.matches;
+
+    this.mediaQueryListener = () => {
+      this.zone.run(() => {
+        this.isPhoneScreen = this.mediaQueryList.matches;
+        this.calculateMarginTop();
+        this.cdr.detectChanges();
+      });
+    };
+
+    this.mediaQueryList.addEventListener('change', this.mediaQueryListener);
+  
+
+  }
+  ngOnDestroy(): void {
+    this.mediaQueryList.removeEventListener('change', this.mediaQueryListener);
+  }
   ngAfterViewInit(): void {
     // console.log(getComputedStyle(this.modelVw.nativeElement).height);
     // const mvHeight = getComputedStyle(this.modelVw.nativeElement).height
-    const px = this.getPX().replace('px','')
-    const padding = parseInt(px)+40;
-    this.marginTop = `${padding}px`
+    // const px = this.getPX().replace('px','')
+    // const padding = parseInt(px)+40;
+    // this.marginTop = `${padding}px`
+    this.zone.runOutsideAngular(() => {
+      this.calculateMarginTop();
+    });
+    
 
     /**
      * Ajuste de nivel de volumen por defecto
      */
     this.audioConfig.nativeElement.volume = 0.18;
-  
+    this.cdr.detectChanges(); // Llamada a detectChanges
+
   }
   ngOnInit(): void {
     this.slug = this.route.snapshot.params['id'];
@@ -65,6 +96,19 @@ export class InstrumentMvComponent implements OnInit, AfterViewInit{
       return getComputedStyle(this.modelVw.nativeElement).height;
     }
     return getComputedStyle(this.imgDetail.nativeElement).height;
+  }
+
+  calculateMarginTop(): void {
+    const px = this.getPX().replace('px', '');
+    let padding = parseInt(px) + 40;
+
+    // Ajuste adicional para tamaños de pantalla más pequeños
+    if (this.isPhoneScreen) {
+      // Ajusta el margen según tus necesidades para tamaños de pantalla más pequeños
+      padding = padding-10;
+    }
+
+    this.marginTop = `${padding}px`;
   }
   // getURLFile(){
   //     this.urlFile = this.instService.getURLFile(this.instrument.glbModel);
